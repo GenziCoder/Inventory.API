399a26931a45fb08abf1f81c637481cf744ef354
Asfahan Tutorial <asfahan.dotnet.tutorials@gmail.com>
2026-08-02 05:17:56 +0530

    Fix CORS

--- Inventory.API/Extensions/CorsExtensions.cs
+++ Inventory.API/Extensions/CorsExtensions.cs
@@ -6,15 +6,19 @@
             this IServiceCollection services,
             IConfiguration configuration)
         {
-            var origins = configuration
-                .GetSection("AllowedOrigins")
-                .Get<string[]>() ?? Array.Empty<string>();
+            //var origins = configuration
+            //    .GetSection("AllowedOrigins")
+            //    .Get<string[]>() ?? Array.Empty<string>();
 
             services.AddCors(options =>
             {
                 options.AddPolicy("AngularPolicy", policy =>
                 {
-                    policy.WithOrigins(origins)
+                    policy.WithOrigins(
+                        
+                        "https://inventory-management-system-silk-ten.vercel.app",
+                        "http://localhost:4200"
+                        )
                           .AllowAnyHeader()
                           .AllowAnyMethod()
                           .AllowCredentials();
